38307cb5a2589cb8114ea1ec9c3cbfb2f8e2a5ba
Josh Ellithorpe <quest@mac.com>
2018-01-22 13:51:49 -0800

    Update ninja.paperwallet.js
    Fix replace for testnet use.

--- src/ninja.paperwallet.js
+++ src/ninja.paperwallet.js
@@ -186,7 +186,7 @@ ninja.wallets.paperwallet = {
 		keyValuePair["qrcode_public" + idPostFix] = bchaddr.toCashAddress(bitcoinAddress);
 		keyValuePair["qrcode_private" + idPostFix] = privateKey;
 		ninja.qrCode.showQrCode(keyValuePair, 2.5);
-		document.getElementById("btcaddress" + idPostFix).innerHTML = bchaddr.toCashAddress(bitcoinAddress).replace(/^bitcoincash:/, '');
+		document.getElementById("btcaddress" + idPostFix).innerHTML = bchaddr.toCashAddress(bitcoinAddress).replace(/^.*:/, '');
 
 		if (ninja.wallets.paperwallet.encrypt) {
 			var half = privateKey.length / 2;
